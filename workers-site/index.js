09b8557bc9c684de9ef6af3635964d9e75ee3bab
Mebebm27 <mebebm27@gmail.com>
2025-06-02 00:14:13 -0600

    update worker script

--- workers-site/index.js
+++ workers-site/index.js
@@ -1,11 +1,11 @@
 export default {
-  async fetch(request) {
-    const baseUrl = new URL(request.url);
+  async fetch(request, env, ctx) {
+    const url = new URL(request.url);
 
-    // Adjust the pathname to point to the correct folder and file
-    baseUrl.pathname = '/index.html';
+    // Adjust to point directly to your static site
+    url.pathname = '/index.html';
 
-    // Serve the `index.html` from the website folder
-    return fetch(baseUrl.toString());
+    // Fetch the `index.html` file from your website bucket
+    return fetch(url.toString());
   },
 };
